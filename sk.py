8b67b4a9b63b7adf4e3a44b474b3a18dc1ac3ce0
shajeb ahmed <shajebahmed@gmail.com>
2025-05-09 12:09:38 +0530

    change

--- sk.py
+++ sk.py
@@ -1,8 +1,8 @@
-#matrix = [[1,5,9],[10,11,13],[12,13,15]]
-#k = 8
-#var = [j for i in matrix for j in i]
-#var.sort()
-#print(var[k-1])
+matrix = [[1,5,9],[10,11,13],[12,13,15]]
+k = 8
+var = [j for i in matrix for j in i]
+var.sort()
+print(var[k-1])
 
 
 
@@ -23,3 +23,14 @@ for i in range(rows-1):
     if not var:
       break
 print(var)
+
+
+
+from itertools import permutations
+input = "abc"
+var = ["".join(i) for i in permutations(input)]
+print(var)
+
+
+
+
